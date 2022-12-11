07ad6b70e1ae4ff73960ca87cb9cc2bf89fc7224
binding1 <jaey.pro@gmail.com>
2022-12-11 09:55:13 +0000

    add character counter script

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  //event listener for text box input
+  $('#tweet-text').on('input', function(e) {
+    //character counter for tweet-text textarea's length
+    let charCounter = $(this).val().length;
+    let remainChar = 140 - charCounter;
+
+    //target counter value
+    let counter = $(this).parent().children('.intext-tweet').children('.counter');
+    //update counter value
+    counter.text(remainChar);
+
+    //change font colour to red when counter exceeds 140 characters
+    if (remainChar < 0) {
+      counter.css('color', 'red');
+    } else {
+      counter.css('color', '#545149');
+    }
+  });
+});
