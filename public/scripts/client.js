c12a4943b955da78e35874a9d88023270168b002
binding1 <jaey.pro@gmail.com>
2022-12-15 06:36:34 +0000

    add tweet append, tweet render and event listener at document ready post request with jQuery

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,72 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+];
 
+const renderTweets = (tweetData) => {
+  for (let i of tweetData) {
+    let $tweet = createTweetElement(tweetData[i]);
+    $('.existing-tweets').append($tweet);
+  }
+};
+
+const createTweetElement = (tweetData) => {
+  const today = new Date();
+  const tweetDate = new Date(tweetData["created_at"]);
+  const tweetDaysAgo = Math.round((today.getTime() - tweetDate.getTime()) / (1000 * 3600 * 24));
+  const $tweet = (
+    `<article class="tweet">
+      <header class="tweet-header">
+        <div class="tweet-user-profile"><img src=${tweetData["user"].avatars}<span class="profile-text">${tweetData["user"].name}</span></div>
+        <div class="profile-tag">${tweetData["user"].handle}</div>
+      </header>
+      <p>${tweetData["content"].text}</p>
+      <footer class="tweet-footer">
+        <span>${tweetDaysAgo + " days ago"}</span>
+        <div class="icons">
+          <i class="fa-solid fa-flag"></i>
+          <i class="fa-solid fa-retweet"></i>
+          <i class="fa-solid fa-heart"></i>
+        </div>
+      </footer>
+    </article>`
+);
+return $tweet;
+};
+
+
+$(document).ready(function() {
+  $('.tweet-submit').submit(function(event) {
+    event.preventDefault();
+    console.log('Tweet added');
+    const serializeData = $(this).serialize();
+
+    $.ajax('/tweets', {
+      method: 'POST',
+      data: serializeData
+    })
+  })
+});
