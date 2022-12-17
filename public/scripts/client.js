27559009b2631a1c9c9bef070f4396dfbb79dc10
binding1 <jaey.pro@gmail.com>
2022-12-17 06:04:11 +0000

    fix bug where tweets weren't properly rendering on page

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,36 +3,11 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-// const data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png"
-//       ,
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": "https://i.imgur.com/nlhLi3I.png",
-//       "handle": "@rd" },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   }
-// ];
-
 
 const renderTweets = (tweetData) => {
   for (let i of tweetData) {
-    let $tweet = createTweetElement(tweetData[i]);
-    $('.existing-tweets').prepend($tweet);
+    let $tweet = createTweetElement(i);
+    $('#tweets-container').append($tweet);
   }
 };
 
