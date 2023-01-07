5a999f39d2a5916d0048d1bff6d20bd8d36dede7
Jaewon Bae <jaewon@Jaewons-MacBook-Pro.local>
2023-01-06 21:00:30 -0500

    fix tweet loading bug after char limit error

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,25 +1,22 @@
-
 //filter HTML forms to prevent injections
-const esc = function(str) {
-  let div = document.createElement("div");
-  div.appendChild(document.createTextNode(str));
-  return div.innerHTML;
+const esc = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
 };
-
 
 //function to prepend new tweets
 const renderTweets = (tweetData) => {
-  for (let i of tweetData) {
-    let $tweet = createTweetElement(i);
-    $("#tweets-container").prepend($tweet);
-  }
+    for (let i of tweetData) {
+        let $tweet = createTweetElement(i);
+        $("#tweets-container").prepend($tweet);
+    }
 };
-
 
 //function to format and create new tweets then add to index.html
 const createTweetElement = (tweetData) => {
-  const tweetDaysAgo = timeago.format(tweetData["created_at"], "en_US");
-  const $tweet = `<article class="tweet">
+    const tweetDaysAgo = timeago.format(tweetData["created_at"], "en_US");
+    const $tweet = `<article class="tweet">
       <header class="tweet-header">
         <div class="tweet-user-profile">
           <img src=${tweetData["user"].avatars}>
@@ -38,71 +35,68 @@ const createTweetElement = (tweetData) => {
       </footer>
     </article>
     </br>`;
-  return $tweet;
+    return $tweet;
 };
-
 
 //load existing tweets, GET request in ajax
 const loadTweets = () => {
-  $.ajax({
-    url: "/tweets",
-    method: "GET",
-  }).then((tweetData) => {
-    renderTweets(tweetData);
-  });
+    $.ajax({
+        url: "/tweets",
+        method: "GET",
+    }).then((tweetData) => {
+        renderTweets(tweetData);
+    });
 };
-
 
 //character counter reset function
 const counterReset = () => {
-  $(".counter").text(140);
+    $(".counter").text(140);
 };
-
 
 //form empties after submit
 const formReset = () => {
-  $("#tweet-text").val("");
+    $("#tweet-text").val("");
 };
-
 
 //error message function
 const formErrorMsg = (errorMsg) => {
-  $("#tweet-error").html(errorMsg).slideDown(700);
+    $("#tweet-error").html(errorMsg).slideDown(700);
 };
 
-
 //jquery new tweet handler
-$(document).ready(function() {
-  loadTweets();
-  $(".tweet-submit").on("submit", function(event) {
-    event.preventDefault();
-    const tweetLength = $(this).children("#tweet-text");
-
-    if (!tweetLength.val()) {
-      formErrorMsg(
-        '<i class="fa-solid fa-triangle-exclamation"></i> Please enter a tweet.'
-      );
-      return false;
-    }
-
-    if (tweetLength.val().length > 140) {
-      formErrorMsg(
-        '<i class="fa-solid fa-triangle-exclamation"></i> Tweet is over the 140 character limit.'
-      );
-      return false;
-    }
-S
-    //animation for error message
-    $("#tweet-error").slideUp(700);
-    
-    const serializeData = $(this).serialize();
-    $.ajax("/tweets", {
-      method: "POST",
-      data: serializeData,
-    });
-
-    counterReset();
-    formReset();
+$(document).ready(function () {
     loadTweets();
-  });
+    $(".tweet-submit").on("submit", function (event) {
+        event.preventDefault();
+        const tweetLength = $(this).children("#tweet-text");
+
+        if (!tweetLength.val()) {
+            formErrorMsg(
+                '<i class="fa-solid fa-triangle-exclamation"></i> Please enter a tweet.'
+            );
+            console.log(errorMsg);
+            return false;
+        }
+
+        if (tweetLength.val().length > 140) {
+            formErrorMsg(
+                '<i class="fa-solid fa-triangle-exclamation"></i> Tweet is over the 140 character limit.'
+            );
+            return false;
+        }
+
+        //animation for error message
+        $("#tweet-error").slideUp(700);
+
+        const serializeData = $(this).serialize();
+        $.ajax("/tweets", {
+            method: "POST",
+            data: serializeData,
+            //functions will occur only after POST request is complete
+        }).then(() => {
+            counterReset();
+            formReset();
+            location.reload();
+        });
+    });
 });
